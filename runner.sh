4b66ea342ab3a1801768b4f81ae075e5b4794054
Kunalb@123 <kunalbiswas241@gmail.com>
2025-05-08 10:34:24 +0530

    test out the all features

--- runner.sh
+++ runner.sh
@@ -16,6 +16,24 @@ echo "THREAD_COUNT  : ${THREAD_COUNT:-2}"
 echo "TEST_SUITE    : ${TEST_SUITE}"
 echo "-------------------------------------------"
 
+# Do not start the tests immediately. Hub has to be ready with browser nodes
+echo "Checking if hub is ready..!"
+count=0
+while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
+do
+  count=$((count+1))
+  echo "Attempt: ${count}"
+  if [ "$count" -ge 60 ]
+  then
+      echo "**** HUB IS NOT READY WITHIN 60 SECONDS ****"
+      exit 1
+  fi
+  sleep 1
+done
+
+# At this point, selenium grid should be up!
+echo "Selenium Grid is up and running. Running the test...."
+
 # Start the java command
 java -cp 'libs/*' \
      -Dselenium.grid.enabled=true \
